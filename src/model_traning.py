8e09a3a679bc9f7495f2f2015d408d32f3fb77b6
himanshau <hs9011016@gmail.com>
2025-02-26 22:44:59 +0530

    third commit of dvc exp. learning

--- src/model_traning.py
+++ src/model_traning.py
@@ -14,8 +14,8 @@ y = df['placed']
 # Split the dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-n_estimators = 100
-max_depth = 10
+n_estimators = 10
+max_depth = 5
 
 # Initialize and train the model
 model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
